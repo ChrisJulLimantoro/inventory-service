639d9206177f62d1ed6ccbb5921a62133eaf9687
ella <brigittaella15@gmail.com>
2025-02-13 19:42:22 +0700

    feat: operation create

--- src/operation/dto/update-operation.dto.ts
+++ src/operation/dto/update-operation.dto.ts
@@ -6,13 +6,15 @@ export class UpdateOperationRequest {
   price: number | null;
   uom: string | null;
   description: string | null;
+  account_id: string | null;
 
-  constructor({ code, name, price, uom, description }) {
+  constructor({ code, name, price, uom, description, account_id }) {
     this.code = code;
     this.name = name;
     this.price = price;
     this.uom = uom;
     this.description = description;
+    this.account_id = account_id;
   }
 
   static schema() {
@@ -22,6 +24,7 @@ export class UpdateOperationRequest {
       price: z.number().nonnegative().optional(),
       uom: z.string().max(15).optional(),
       description: z.string().max(255).nullable().optional(),
+      account_id: z.string().uuid().optional().nullable(),
     });
   }
 }
