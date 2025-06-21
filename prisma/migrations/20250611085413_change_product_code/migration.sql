aee14ce0af82c3988e543bcf5477f373451b5516
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2025-06-21 22:32:52 +0700

    fix: migrateion

--- prisma/migrations/20250611085413_change_product_code/migration.sql
+++ prisma/migrations/20250611085413_change_product_code/migration.sql
@@ -1,3 +1,4 @@
 -- AlterTable
-ALTER TABLE "product_codes" ADD COLUMN     "certificate_link" TEXT,
+ALTER TABLE "product_codes" 
+-- ADD COLUMN     "certificate_link" TEXT,
 ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT true;
